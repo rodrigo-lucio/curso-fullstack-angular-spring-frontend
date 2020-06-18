029edf8acbd1f6837664152783c2b9af623b4c50
Rodrigo Lucio <luciodigo@gmail.com>
2020-06-17 23:49:23 -0300

    novos arquivos

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -31,13 +31,13 @@ export function tokenGetter(): string {
       config: {
 
         tokenGetter: tokenGetter, //Pega do metodo la em cima que pega do localStorage
-        /* DESCOMENTAR AQUI PARA PRODUÇÃO
+
         whitelistedDomains: ['result-app.herokuapp.com'],
         blacklistedRoutes: ['https://result-app.herokuapp.com/oauth/token']
-        */
+        /* DESCOMENTAR AQUI PARA PRODUÇÃO
          whitelistedDomains: ['localhost:8080'],
          blacklistedRoutes: ['http://localhost:8080/oauth/token']
-
+*/
       }
     }),
 
